c47efeb9d039f30fcc7d7b8b30db0709489a6b2b
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-20 07:20:46 -0500

    Updated UI and JS for adding a book

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+addBook = document.querySelector("#add-book");
+containerTwo = document.querySelector("#container-two");
+overlayDiv = document.querySelector("#overlay");
+form = document.querySelector("form");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -10,3 +15,23 @@ function Book(title, author, pages, read) {
 const addBookLibrary = () => {
   myLibrary.forEach((book) => {});
 };
+
+const addBookPress = (e) => {
+  if (e.target === addBook) {
+    overlayDiv.style.display = "flex";
+  }
+};
+
+const formSubmitted = () => {
+  overlayDiv.style.display = "none";
+};
+
+const removeOverlay = (e) => {
+  if (e.target === e.currentTarget) {
+    overlayDiv.style.display = "none";
+  }
+};
+
+containerTwo.addEventListener("click", addBookPress);
+form.addEventListener("submit", formSubmitted);
+overlayDiv.addEventListener("click", removeOverlay);
